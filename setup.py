4ad93046ba0998ae475d7027bc154e8ba0583264
Bryan Bishop <kanzure@gmail.com>
2012-11-25 23:26:13 -0600

    turn this into a basic python egg

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+version = "1.0.0"
+
+setup(name="googleplayapi",
+      version=version,
+      author="Emilien Girault",
+      author_email="emilien.girault@gmail.com",
+      license="BSD",
+      description="Google Play API for Android",
+      url="https://github.com/kanzure/googleplay-api",
+      packages=find_packages())
