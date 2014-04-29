f9bdefbbcbaa1fba29fdf9903c172a88c06f5100
Ryan Ye <ryan@upwlabs.com>
2014-04-29 15:19:18 +0800

    remove force check for values in config.py

--- googleplayapi/config.py
+++ googleplayapi/config.py
@@ -5,8 +5,3 @@ ANDROID_ID      = None # "xxxxxxxxxxxxxxxx"
 GOOGLE_LOGIN    = None # "username@gmail.com"
 GOOGLE_PASSWORD = None
 AUTH_TOKEN      = None # "yyyyyyyyy"
-
-# force the user to edit this file
-if any([each == None for each in [ANDROID_ID, GOOGLE_LOGIN, GOOGLE_PASSWORD]]):
-    raise Exception("config.py not updated")
-
